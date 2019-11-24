c514f1bdc5ac8a48d35996d8af8e7e5b1110e839
Semyon Volkov <s.volkov@xsolla.com>
2019-11-24 18:31:45 +0500

    initial commit

--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
-const express = require('express');
+import express, { Application, Request, Response } from 'express';
 
-const app = express();
+const app: Application = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req: Request, res: Response) => res.send('Hello World!'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
